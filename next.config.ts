d5bceda77de0b20558474b6a91abe6b5ef48d124
小刘同学的gitee <460412664@qq.com>
2025-08-14 10:33:13 +0800

    提交build后的docs，用于构建文档web

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  // 输出到 docs 文件夹
+  distDir: 'docs',
+  // 根路径部署
+  assetPrefix: '',
+  basePath: '',
 };
 
 export default nextConfig;
