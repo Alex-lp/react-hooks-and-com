da04fc56b397e588bfedcb04da27e6cc4547c7d6
小刘同学的gitee <460412664@qq.com>
2025-08-14 11:02:20 +0800

    重新设置baseurl并打包

--- next.config.ts
+++ next.config.ts
@@ -7,11 +7,12 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true
   },
+  // 设置基础路径为你的仓库名
+  basePath: '/react-hooks-and-com',
+  // 设置资源前缀
+  assetPrefix: '/react-hooks-and-com/',
   // 输出到 docs 文件夹
   distDir: 'docs',
-  // 根路径部署
-  assetPrefix: '',
-  basePath: '',
 };
 
 export default nextConfig;
