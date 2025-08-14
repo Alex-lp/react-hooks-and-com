ddbab10dcaad0342892e9bba8820c702c0168ecc
小刘同学的gitee <460412664@qq.com>
2025-08-14 11:58:37 +0800

    取消全路径pages前缀

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
   // 设置基础路径为你的仓库名
   basePath: '/react-hooks-and-com',
   // 设置资源前缀
-  assetPrefix: 'https://raw.githubusercontent.com/Alex-lp/react-hooks-and-com/refs/heads/main/docs/',
+  assetPrefix: '/react-hooks-and-com/',
   // 输出到 docs 文件夹
   distDir: 'docs',
 };
